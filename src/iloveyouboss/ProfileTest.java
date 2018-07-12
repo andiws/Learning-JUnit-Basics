0197997af0b15f95dbea4014bfea1336e79b2482
Andi <garcia.andrea125@gmail.com>
2018-07-12 09:28:09 -0500

    Adding @Before annotations nothing broke

--- src/iloveyouboss/ProfileTest.java
+++ src/iloveyouboss/ProfileTest.java
@@ -9,13 +9,22 @@
 package iloveyouboss;
 
 import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*; 
+import static org.junit.Assert.*; 
+
+/*We want the tests to stay short and
+clear. Both our tests instantiate Profile. Create a Profile field and move the
+common initialization to an @Before method*/
 
 public class ProfileTest {
+	 
+	private Profile profile;
+	
+	@Before
+	public void createProfile() {
+		 profile = new Profile(); 
+	}
 	@Test
 	public void matchesNothingWhenProfileEmpty() {
-		Profile profile = new Profile();
 		Question question = new BooleanQuestion(1, "Relocation package?");
 		Criterion criterion = 
 				new Criterion(new Answer(question,Bool.TRUE), Weight.DontCare); 
@@ -27,7 +36,7 @@ public class ProfileTest {
 	}
 	@Test
 	public void matchesWhenProfileContainsSameAnswerfromCriterion() {
-		Profile profile = new Profile();
+
 		Question question = new BooleanQuestion(1, "Relocation package?");
 		Answer answer = new Answer(question, Bool.TRUE); 
 		profile.add(answer); 
